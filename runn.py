f750cf35f83e86fe3865889c4d219e7c4da77e5d
leiyong711 <leiyong711@163.com>
2017-07-26 16:11:24 +0800

    完成基本功能

---
+++ runn.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# project name: Interface-test
+# author: "Lei Yong" 
+# creation time: 2017/7/26 0026 16:07
+# Email: leiyong711@163.com
+from Interface.excel_w import apc
+# from Public.html import create
+asb = apc()
+print asb.exceslw()
